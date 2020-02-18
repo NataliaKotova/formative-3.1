24cae15e283b421b947e287e9c7dc899e05277a7
Natalia Kotova <natalia.v.kotova@gmail.com>
2020-02-19 10:30:13 +1300

    updated script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,59 @@
-console.log('npm-grunt');
 $(document).ready(function(){
-  //to check if jquery module is working
-  $(this).css('background', 'cyan');
-});
+  var myKey = JSON.parse(apiKey);  
+  console.log(myKey[0]);
+  myKey = myKey[0].key;
+  console.log(myKey);
+
+  // var url = 'http://newsapi.org/v2/top-headlines?'+ 
+  // 'country=nz&'+ 
+  // 'apiKey=' + myKey;
+
+  var url = 'http://newsapi.org/v2/everything?'+ 'q=all&' + 'apiKey=' + myKey;
+  // 'country=nz&'+ 
+  
+
+  console.log(url);
+
+  function displayAllNews(array){
+    var i;
+    document.getElementById('result').innerHTML = '';
+    for(i=0; i<array.length; i++){
+      if(array[i].urlToImage === null){
+        array[i] = array[i+1];
+      }
+      document.getElementById('result').innerHTML +=
+        `<div class="card col-lg-4 mb-5">
+        <img src="${array[i].urlToImage}" class="card-img-top" alt="Image">
+        <div class="card-body">
+          <h5 class="card-title"><a href="${array[i].url}" target="blank">${array[i].title}</a></h5>
+          <p class="card-text">${array[i].description}</p>
+          ${/* <a href="${array[i].url}" target="blank" class="btn btn-primary d-inline">Read</a>*/''}
+          <div class"d-inline">
+          <small class="d-inline">${array[i].source.name}</small>
+          <small class="d-inline">${array[i].source.name}</small>
+        </div>
+      </div>`;
+    }
+  };
+
+  var req = new Request(url);
+      fetch(req)
+          .then(function (response) {
+              console.log(response.json());
+          });
+
+      $.ajax({
+        url: url,
+        type:'GET',
+        data:'json',
+        success: function(data){
+          console.log(data);
+          console.log(data.articles[0]);
+          displayAllNews(data.articles);
+        }, 
+        error: function(){
+          console.log('error');
+        } 
+    }); //ajax
+
+});//document.ready
