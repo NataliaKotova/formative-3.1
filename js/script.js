8c4a6070bc499cec658fd4795f2550709b162fb6
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-02-25 00:21:45 +0000

    edited search news by source

--- js/script.js
+++ js/script.js
@@ -8,11 +8,16 @@ $(document).ready(function(){
   document.getElementById('submit1').addEventListener('click', function(){
       var country = document.getElementById('inputCountry').value;
       var category = document.getElementById('inputCategory').value;
-      // var source = document.getElementById('inputSource').value;
-  //call functions set#1
       saveData(country, category);
       var url = buildUrl();
       queryApi(url);
+  });
+
+  document.getElementById('submit2').addEventListener('click', function(){
+    var valueSelectedSource = $("#inputSource option:selected").html();
+    saveDataSource();
+    console.log(valueSelectedSource);
+    console.log(typeof(valueSelectedSource));
   });
 });//document.ready
 
@@ -23,12 +28,13 @@ function queryApi(url){
     type:'GET',
     data:'json',
     success: function(data){
-      // console.log(data);
+      console.log(data);
       console.log(data.articles[0]);
       // console.log(data.articles[0].source);
       //call function to create array with sources object
-      // createSourceList(data.articles);
+      createSourceList(data.articles);
       displayAllNews(data.articles);
+      displayNewsBySource(data.articles);
     },
     error: function(){
       console.log('error');
@@ -56,6 +62,17 @@ function saveData(country, category){
   
   console.log(userInput);
 }
+
+function saveDataSource(source){
+  if(source !== ""){
+    var valueSelectedSource = $("#inputSource option:selected").html();
+    userInput.sourceCode = valueSelectedSource;
+  }
+  
+  console.log(userInput);
+}
+
+
 //==============================================================================
 function buildUrl(){
   var url = 'http://newsapi.org/v2/top-headlines?' + 'apiKey=' + myKey;
@@ -68,6 +85,18 @@ function buildUrl(){
   console.log(url);
   return url;
 }
+
+function buildUrlSource(){
+  var url = 'http://newsapi.org/v2/top-headlines?' + 'apiKey=' + myKey;
+  
+  if(userInput.categoryCode != null){
+    url +=  '&category=' + userInput.categoryCode;
+  }
+  console.log(url);
+  return url;
+}
+
+
 
 var sources = [];
 var articles = [];
@@ -94,7 +123,6 @@ function createSourceList(arr){
 function selectCountryOnChange(){
   $('#inputCountry').on('change', function(){
     userInput.countryCode = this.value;
-    // createSourceList(arr);
     console.log(userInput.countryCode);
   });
 }
@@ -109,7 +137,7 @@ function displayAllNews(array){
       array[i] = array[i+1];
     }
     document.getElementById('result').innerHTML +=
-      '<div class="col-md-3 col-lg-3 mb-5">'+
+      '<div class="col-md-4 col-lg-4 mb-5">'+
       '<div class="card">'+
       '<img src="'+ array[i].urlToImage +'" class="card-img-top" alt="Image">'+
       '<div class="card-body">'+
@@ -122,6 +150,30 @@ function displayAllNews(array){
     '</div>';
   }
 };
+
+function displayNewsBySource(array){
+  var i;
+  // document.getElementById('result2').innerHTML = '';
+  for(i=0; i<array.length; i++){
+    if(array[i].source.name === userInput.sourceCode){
+      sources[i] = array[i];
+      console.log(sources);
+    }
+    document.getElementById('result2').innerHTML +=
+      '<div class="col-md-4 col-lg-4 mb-5">'+
+      '<div class="card">'+
+      '<img src="'+ array[i].urlToImage +'" class="card-img-top" alt="Image">'+
+      '<div class="card-body">'+
+        '<h5 class="card-title"><a href="'+ array[i].url + '" target="blank">'+array[i].title + '</a></h5>'+
+        '<p class="card-text">'+array[i].description+'</p>'+
+        '<div class"d-inline">'+
+        '<small class="d-inline">Source: '+array[i].source.name+'</small>'+
+      '</div>'+
+    '</div>'
+    '</div>';
+  }
+};
+
 
 function testApiKey(apiKey){
   var url = 'http://newsapi.org/v2/top-headlines?'+
